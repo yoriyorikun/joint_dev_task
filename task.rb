ff200c7eff150b597d1a397816020f86c12d73b5
青木亮隆 <yori-goreng@aokiryoutakashinoMacBook-puro.local>
2020-04-19 21:22:41 +0900

    質問8..10を回答

--- task.rb
+++ task.rb
@@ -68,6 +68,9 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
+  programming_languages = programming_languages.map{|language| language.capitalize}
+  upper_case_programming_languages = %w(ruby php python javascript)
+  upper_case_programming_languages = upper_case_programming_languages.map{|upper| upper.upcase}
 
   # 以下は変更しないで下さい
   p programming_languages
@@ -78,6 +81,9 @@ def q9
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
+  names.each.with_index(1) do |name, i|
+    puts "会員No.#{i} #{name}さん"
+  end
 
 end
 
@@ -85,7 +91,12 @@ def q10
   foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
 
   # 以下に回答を記載
-
+  if foods.include?("うに")
+    p "好物です"
+  else
+    p "まぁまぁ好きです"
+  end
+  
 end
 
 def q11
